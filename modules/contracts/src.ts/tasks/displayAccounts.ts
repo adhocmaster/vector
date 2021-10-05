15a0d6f6210bb3acf5bff0a65e6a59cbc8370acb
Adhocmaster <adhocmaster@live.com>
2021-10-04 17:47:30 -0700

    fixed display-accounts task in contracts module

--- modules/contracts/src.ts/tasks/displayAccounts.ts
+++ modules/contracts/src.ts/tasks/displayAccounts.ts
@@ -6,21 +6,28 @@ import pino from "pino";
 export default task("display-accounts", "Displays first 3 accounts and their recommended gifts")
   .addParam("mnemonic", "The mnemonic to display accounts for")
   .addOptionalParam("logLevel", "One of 'debug', 'info', 'warn', 'error', 'silent' (default: info)")
-  .setAction(async (args): Promise<void> => {
+  .setAction(async (args, hre): Promise<void> => {
     const { mnemonic, logLevel } = args;
     const log = pino({ level: logLevel || "info" });
-    const hdNode = HDNode.fromMnemonic(mnemonic).derivePath("m/44'/60'/0'/0");
-    const wallets: Wallet[] = Array(20)
-      .fill(0)
-      .map((_, idx) => {
-        const wallet = new Wallet(hdNode.derivePath(idx.toString()).privateKey);
-        return wallet;
-      });
-    log.info(
-      { wallets: wallets.map(w => w.address), privateKeys: wallets.map(w => w.privateKey) },
-      "All contract testing accounts",
-    );
-    log.info({ alice: wallets[0].address, recommended: "1 ETH" }, "Alice");
-    log.info({ bob: wallets[1].address, recommended: "0.5 ETH" }, "Bob");
-    log.info({ rando: wallets[2].address, recommended: "0.1 ETH" }, "Rando");
+    // const hdNode = HDNode.fromMnemonic(mnemonic).derivePath("m/44'/60'/0'/0");
+    // const wallets: Wallet[] = Array(20)
+    //   .fill(0)
+    //   .map((_, idx) => {
+    //     const wallet = new Wallet(hdNode.derivePath(idx.toString()).privateKey);
+    //     return wallet;
+    //   });
+    // log.info(
+    //   { wallets: wallets.map(w => w.address), privateKeys: wallets.map(w => w.privateKey) },
+    //   "All contract testing accounts",
+    // );
+    // log.info({ alice: wallets[0].address, recommended: "1 ETH" }, "Alice");
+    // log.info({ bob: wallets[1].address, recommended: "0.5 ETH" }, "Bob");
+    // log.info({ rando: wallets[2].address, recommended: "0.1 ETH" }, "Rando");
+
+    const namedAccounts = await hre.getNamedAccounts()
+    
+    for (let name in namedAccounts) {
+      log.info(`name: ${name}, address ${namedAccounts[name]}`)
+    }
+
   });
